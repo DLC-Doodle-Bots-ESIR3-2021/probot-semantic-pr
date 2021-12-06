4ab52fb0775bd61b67832b429d8d305abc3659ba
Hugo-D <hugo.denis.35@gmail.com>
2021-12-06 17:39:14 +0100

    Clean workspace and add conditions if merged

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,20 @@ import { Probot } from "probot";
 export = (app : Probot) => {  
   app.log.info("Probot-semantic-pr is running")   
   /** handle depreacated tags in the andi PR's */        
-  app.on(["pull_request_review.submitted",
-  "pull_request_review.edited",
-  "pull_request_review.dismissed"],
+  app.on("pull_request_review.submitted",
   async context => {
-    const required_num_prr = 2
-    const pr_num:number = context.payload.pull_request.number
-    const pr_node_id:string = context.payload.pull_request.node_id
+    if (context.payload.pull_request.merged_at === null) {
+      const required_num_prr = 1
+      const pr_num:number = context.payload.pull_request.number
+      const pr_node_id:string = context.payload.pull_request.node_id
 
-    const request_num_prr:any = await context.octokit.graphql(`query { 
-      repository(owner:"DLC-Doodle-Bots-ESIR3-2021", name:"doodle") { 
-        pullRequest(number: ${pr_num}) {
-          reviews(first: 100, states: APPROVED) {
-            totalCount
-          }
-        }
-      }
-    }`,
-    {
-      headers: {
-        authorization: `token ${process.env.GITHUB_PAT}`,
-      },
-    })
-
-    const num_prr:number = request_num_prr.repository.pullRequest.reviews.totalCount
-    if (num_prr >= required_num_prr) {
-      await context.octokit.graphql(`mutation MergePullRequest { 
-        mergePullRequest (input: {
-          pullRequestId: "${pr_node_id}",
-        }) {
-          pullRequest {
-            number
+      const request_pr:any = await context.octokit.graphql(`query { 
+        repository(owner:"DLC-Doodle-Bots-ESIR3-2021", name:"doodle") { 
+          pullRequest(number: ${pr_num}) {
+            merged
+            reviews(first: 100, states: APPROVED) {
+              totalCount
+            }
           }
         }
       }`,
@@ -43,9 +26,32 @@ export = (app : Probot) => {
         },
       })
 
-      app.log.info("Pull request merged")
-    } else {
-      app.log.info(`Pull request doesn't have enough reviews approved (${num_prr} approved for ${required_num_prr} required)`)
+      const merged:boolean = request_pr.repository.pullRequest.merged
+      const num_prr:number = request_pr.repository.pullRequest.reviews.totalCount
+      if (!merged) {
+        if (num_prr >= required_num_prr) {
+          await context.octokit.graphql(`mutation MergePullRequest { 
+            mergePullRequest (input: {
+              pullRequestId: "${pr_node_id}",
+            }) {
+              pullRequest {
+                number
+              }
+            }
+          }`,
+          {
+            headers: {
+              authorization: `token ${process.env.GITHUB_PAT}`,
+            },
+          })
+          
+          app.log.info("Pull request merged")
+        } else {
+          app.log.info(`Pull request doesn't have enough reviews approved (${num_prr} approved for ${required_num_prr} required)`)
+        }
+      } else {
+        app.log.info(`Pull request is already merged`)
+      }
     }
   })
 };
--- src/pull-request-check.ts
+++
@@ -1,5 +0,0 @@
-import { Context }  from "probot";
-export const checkDepreacatedTags = async (context: Context) => {
-    const response = await context.pullRequest.toString()
-    console.log(response)
-}
